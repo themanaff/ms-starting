e51a99649be198d64460b2df00a245bf0b2f0037
themanaff <ahmedovmenaf@gmail.com>
2024-03-01 16:53:41 +0400

    Scheduler ile islemek ucun ilk usulu yazdiq

--- ms-payment/src/main/java/com/example/ms/payment/cron/MoneyTransferCron.java
+++ ms-payment/src/main/java/com/example/ms/payment/cron/MoneyTransferCron.java
@@ -4,6 +4,7 @@ import com.example.ms.payment.service.PaymentService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -12,13 +13,17 @@ import org.springframework.stereotype.Component;
 @FieldDefaults(level = AccessLevel.PACKAGE,makeFinal = true)
 public class MoneyTransferCron {
     PaymentService paymentService;
-//    @Scheduled(fixedDelayString = "PT10S")//every 10seconds
-//    public void updateAnyData() {
-//        paymentService.sendPaymentToAzericard();
-//    }
-
-    @Scheduled(cron = "${scheduler.cron.expired-something}")
+    @Scheduled(fixedDelayString = "PT10S")//every 10seconds
+    @SchedulerLock(name = "update-any-info-in-something",
+            lockAtLeastFor = "PT15S",
+            lockAtMostFor = "PT30M"
+    )
     public void updateAnyData() {
         paymentService.sendPaymentToAzericard();
     }
+
+//    @Scheduled(cron = "${scheduler.cron.expired-something}")
+//    public void updateAnyData() {
+//        paymentService.sendPaymentToAzericard();
+//    }
 }
