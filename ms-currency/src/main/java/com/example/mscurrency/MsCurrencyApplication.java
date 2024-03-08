b02dc38be61724366aad1f4805f6945c77158573
themanaff <ahmedovmenaf@gmail.com>
2024-03-08 17:23:13 +0400

    Currency class yaradildi

---
+++ ms-currency/src/main/java/com/example/mscurrency/MsCurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mscurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCurrencyApplication.class, args);
+	}
+
+}
